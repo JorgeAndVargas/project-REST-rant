114fd96405e4165fff971b5243bdca74d547af9d
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2023-02-11 03:16:10 -0500

    Parts 11 and 12

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -24,7 +24,7 @@ function index (data) {
     return (
       <Def>
           <main>
-              <h1>Places to Rant or Rave About</h1>
+              <h1>Places to Rant or Rave About:</h1>
               <div className="row">
                 {placesFormatted}
               </div>
